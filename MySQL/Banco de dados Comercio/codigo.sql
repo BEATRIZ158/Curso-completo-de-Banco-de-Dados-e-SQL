051ec507326f3c8d6523ffeb3e7351c9c3dad1dc
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-19 14:07:33 -0300

    Código

--- MySQL/Banco de dados Comercio/codigo.sql
+++ MySQL/Banco de dados Comercio/codigo.sql
@@ -1,3 +1,5 @@
+/*Modelagem Física*/
+
 /*Criando Banco de Dados*/
 CREATE DATABASE COMERCIO;
 
@@ -7,12 +9,29 @@ SHOW DATABASES;
 /*Acessando Banco de Dados*/
 USE COMERCIO;
 
+/*; É um delimitador*/
 CREATE TABLE CLIENTE(
     /*Chave Primárias, deve ser ÚNICA, cada registro tem uma chave*/
     IDCLIENTE INT PRIMARY KEY AUTO_INCREMENT, /*O número da chave vai se auto incrementar, 1 - 2 - 3 ...., o banco controla*/
     NOME VARCHAR(30) NOT NULL, /*Valor não pode ser NULO (Vasio)*/
-    SEXO ENUM('M', 'F')  NOT NULL,/*Dominío, uma combo box, tem informações fixas, não mudam é algo definido*/
+    SEXO ENUM('M', 'F') NOT NULL,/*Dominío, uma combo box, tem informações fixas, não mudam é algo definido*/
     EMAIL VARCHAR(50) UNIQUE, /*Garante que todos os valores em uma coluna sejam diferentes*/
     CPF VARCHAR(15) UNIQUE
 );
 
+/*Visualizar todas as tabelas criadas no Banco*/
+SHOW TABLES;
+
+CREATE TABLE ENDERECO(
+    IDENDERECO INT PRIMARY KEY AUTO_INCREMENT;
+    RUA VARCHAR(30) NOT NULL,
+    BAIRRO VARCHAR(30) NOT NULL,
+    CIDADE VARCHAR(30) NOT NULL,
+    ESTADO CHAR(2) NOT NULL
+);
+
+CREATE TABLE TELEFONE(
+    IDTELEFONE INT PRIMARY KEY AUTO_INCREMENT,
+    TIPO ENUM('RES', 'COM', 'CEL') NOT NULL,
+    NUMERO VARCHAR(10) NOT NULL
+);
