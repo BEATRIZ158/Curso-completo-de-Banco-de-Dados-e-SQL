eabcf0fde9706bccde8a9139d63c024145663961
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-22 21:03:57 -0300

    Comandos DDL

--- MySQL/Categorias de Comandos/DDL - Modificando Tabelas/codigo.sql
+++ MySQL/Categorias de Comandos/DDL - Modificando Tabelas/codigo.sql
@@ -2,3 +2,73 @@
   DDL - DATA DEFINITION LANGUAGE
 */
 
+CREATE TABLE PRODUTO(
+	IDPRODUTO INT PRIMARY KEY AUTO_INCREMENT,
+	NOME_PRODUTO VARCHAR(30) NOT NULL,
+	PRECO INT,
+	FRETE FLOAT(10,2) NOT NULL
+);
+
+/* ALTER TABLE */
+
+/* ALTERANDO O NOME DE UMA COLUNA - CHANGE */
+
+ALTER TABLE PRODUTO
+CHANGE PRECO VALOR_UNITARIO INT NOT NULL;
+/*Antigo Nome / Novo Nome*/
+
+DESC PRODUTO;
+
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int(11)     | NO   |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+ALTER TABLE PRODUTO
+CHANGE VALOR_UNITARIO VALOR_UNITARIO INT;
+
+DESC PRODUTO;
+
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int(11)     | YES  |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+/* MODIFY - ALTERANDO O TIPO */
+
+ALTER TABLE PRODUTO
+MODIFY VALOR_UNITARIO VARCHAR(50) NOT NULL;
+/*Mais prático usar CHANGE para mudar nome da coluna
+E MODIFY para mudar o tipo do dado
+MAS AMBOS FUNCIONAM, A DIFERENÇA ESTÁ NA SINTAXE*/
+
+/* ADICIONANDO COLUNAS */
+
+ALTER TABLE PRODUTO
+ADD PESO FLOAT(10,2) NOT NULL;
+
+/* APAGANDO UMA COLUNA */
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+/* ADICIONANDO UMA COLUNA NA ORDEM ESPECIFICA */
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+AFTER NOME_PRODUTO; /*Depois de NOME_PRODUTO e Antes de VALOR_UNITARIO*/
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+FIRST;
