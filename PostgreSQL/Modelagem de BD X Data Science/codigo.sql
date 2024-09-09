dd3856831b6ff3c0521314523beecb08b9938bcd
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-09-09 17:00:58 -0300

    Modelagem de BD X Data Science

---
+++ PostgreSQL/Modelagem de BD X Data Science/codigo.sql
@@ -0,0 +1,10 @@
+/* Modelagem Banco de Dados X Data Science */
+
+/* Banco de Dados -> 1,2 e 3 Formas Normais
+Evitam redundancia, consequentemente poupam espaço em disco.
+Consomem muito processamento em função de joins. Queries lentas 
+
+Data Science e B.I -> Focam em agregações e performance, Não se
+preocupam com espaõ em disco. Em B.I, modelagem minima (DW)
+em Data Science, prefencialmente modelagem colunar
+*/
